f21b256402ab83d14e85b793c3fbeb410ed46dab
Kirill Alexander Khalitov <voronar@gmail.com>
2020-05-17 15:25:09 +0300

    provider, context, hooks

--- main.ml
+++ main.ml
@@ -1,5 +1,34 @@
 open Js_of_ocaml
 
+let ctx = React.create_context 1
+
+module NumberProvider: React.PROVIDER with type v = int = struct
+  type v = int [@@js]
+  type t = {
+    value: v;
+  }[@@js]
+  let provider = React.get_provider ctx
+end
+
+module ChildProps = struct
+  type t = {
+    onClick: unit -> unit
+  }[@@js]
+end
+
+module Child: React.FC with type t = ChildProps.t = struct
+  type t = ChildProps.t [@@js]
+
+  let make ChildProps.{onClick} ch = let open React in
+    let c = use_context ctx in
+    let _ = Global.console_log c in
+    div [] [
+      div [Html.Attr.OnClick (fun _ ->
+        onClick ()
+      )] [ch; int c]
+    ]
+end
+
 module EntryPageProps = struct
   type t = {
     text: string;
@@ -9,12 +38,16 @@ end
 module EntryPage: React.FC with type t = EntryPageProps.t = struct
   type t = EntryPageProps.t [@@js]
 
-  let make _props ch =
-    React.div [] [ch]
+  let make _props ch = let open React in
+    let (value, set_state) = use_state (fun _ -> 1) in
+    let onClick () = set_state (fun s -> s + 1) in
+    provider (module NumberProvider) value [
+      fc (module Child) {onClick} [ch]
+    ]
 end
 
 let app: <foo: Js.js_string Js.t Js.prop> Js.t  = Js.Unsafe.js_expr {|require("./../../app.js")|}
 let t = (app##.foo) |> Js.to_string
-let el = React.fc (module EntryPage) {text=t} [React.string "you";React.string " I"]
+let el = React.fc (module EntryPage) {text=t} [React.string "comps"]
 let _ = React.Dom.render el (Dom_html.getElementById "app")
-let _ = Global.console_log app##.foo
+let _ = Global.console_log el
--- react.ml
+++ react.ml
@@ -1,13 +1,36 @@
 open Js_of_ocaml
+module NullOpt = Js.Opt
+module UndefOpt = Js.Optdef
 
 let react_module = Js.Unsafe.js_expr {|require("react")|}
 let react_dom_module = Js.Unsafe.js_expr {|require("react-dom")|}
-let createElement = Js.Unsafe.get react_module "createElement"
-let render = Js.Unsafe.get react_dom_module "render"
+let r_createElement = Js.Unsafe.get react_module "createElement"
+let r_useRef = Js.Unsafe.get react_module "useRef"
+let r_useEffect = Js.Unsafe.get react_module "useEffect"
+let r_useState = Js.Unsafe.get react_module "useState"
+let r_useReducer = Js.Unsafe.get react_module "useReducer"
+let r_useContext = Js.Unsafe.get react_module "useContext"
+let r_createContext = Js.Unsafe.get react_module "createContext"
+let r_fragment = Js.Unsafe.get react_module "Fragment"
+let rd_render = Js.Unsafe.get react_dom_module "render"
 
 type element
 type fragment_component
 type class_component
+type 'a context_js
+type 'a ctx_provider
+
+module type PROVIDER = sig
+  type v
+  type t = {
+    value: v;
+  }
+  val provider: v ctx_provider
+  val t_to_js: t -> Ojs.t
+  val t_of_js: Ojs.t -> t
+  val v_to_js: v -> Ojs.t
+  val v_of_js: Ojs.t -> v
+end
 
 module type FC = sig
   type t
@@ -16,92 +39,221 @@ module type FC = sig
   val make: t -> element -> element
 end
 
-type 'a component =
+type ('a, 'b) component =
   | HtmlTagComponent of string
   | FragmentComponent of fragment_component
+  | ProviderComponent of 'b ctx_provider
   | ClassComponent of class_component
   | FunctionalComponent of ('a -> element -> element)
 
+let parse_children = let open Js.Unsafe in function
+  | [] -> inject Js.Optdef.empty
+  | [ch] -> inject ch
+  | ch -> ch |> Array.of_list |> Js.array |> inject
+
 let create_element ~component ~props ~(children: element list) ~props_of_js : element =
+  let open Js.Unsafe in
   let comp = match component with
-    | HtmlTagComponent s -> Js.Unsafe.inject @@ Js.string s
-    | FragmentComponent frc -> Js.Unsafe.inject frc
-    | ClassComponent cc -> Js.Unsafe.inject cc
+    | HtmlTagComponent s -> inject @@ Js.string s
+    | FragmentComponent frc -> inject frc
+    | ClassComponent cc -> inject cc
+    | ProviderComponent fn -> inject fn
     | FunctionalComponent fn ->
       let props_of_js = Option.get props_of_js in
-      Js.Unsafe.inject @@ Ojs.fun_to_js 1 (fun p ->
-        let c = Js.Unsafe.get p "children" in
+      inject @@ Ojs.fun_to_js 1 (fun p ->
+        let c = get p "children" in
         let p = props_of_js p in
         fn p c)
   in
-  Js.Unsafe.fun_call createElement [|comp;
-    Js.Unsafe.inject props;
-    children |> Array.of_list |> Js.array |> Js.Unsafe.inject;
+  fun_call r_createElement [|comp;
+    inject props;
+    children |> parse_children;
   |]
 
-module Attr = struct
-    type mouse_event = <
-      nativeEvent: Dom_html.mouseEvent Js.t Js.readonly_prop;
-      preventDefault: unit -> unit Js.meth
-    > Js.t
-    type keyboard_event = <
-      nativeEvent: Dom_html.keyboardEvent Js.t Js.readonly_prop;
-      preventDefault: unit Js.meth
-    > Js.t
+(* Context *)
+let create_context (dft_value: 'a): 'a context_js = Js.Unsafe.(fun_call r_createContext [|inject dft_value|])
+let mk_ctx_value v = object%js val value = v end
+let get_provider (ctx_js: 'a context_js): 'a ctx_provider = Js.Unsafe.get ctx_js "Provider"
+let use_context (ctx: 'a context_js): 'a = Js.Unsafe.(fun_call r_useContext [|inject ctx|])
 
-    type 'a attr =
-      | OnClick of (mouse_event -> unit)
-      | OnContextMenu of (mouse_event -> unit)
-      | OnInput of (keyboard_event -> unit)
-      | OnKeyDown of (keyboard_event -> unit)
-      | OnKeyUp of (keyboard_event -> unit)
-      | Key of string
-      (* | Ref of 'a Js.Opt.t ref_object *)
-      | Value of string
-      | Style of Dom_html.cssStyleDeclaration Js.t
-      | ClassName of string
+(* Muttable refs *)
+type 'a mut_ref_object = < current: 'a Js.prop > Js.t
+let ref_current = "current"
+let (!) (ref: 'a mut_ref_object): 'a = Js.Unsafe.get ref ref_current
+let (:=) (ref: 'a mut_ref_object) (v: 'a): unit = Js.Unsafe.set ref ref_current v
+
+let use_ref (value: 'a): 'a mut_ref_object =
+  Js.Unsafe.(fun_call r_useRef [|inject value|])
+
+module Html = struct
+  module InlineStyle = struct
+    type style =
+      | Display of string
+      | Width of string
+      | Height of string
+      | BackgroundColor of string
+      | Color of string
+      | TextDecoration of string
 
     let props_to_attr = function
-      | OnClick fn -> ("onClick", Js.Unsafe.inject @@ Js.Unsafe.callback fn)
-      | OnContextMenu fn -> ("onContextMenu", Js.Unsafe.inject @@ Js.Unsafe.callback fn)
-      | OnInput fn -> ("onChange", Js.Unsafe.inject @@ Js.Unsafe.callback fn)
-      | OnKeyDown fn -> ("onKeyDown", Js.Unsafe.inject @@ Js.Unsafe.callback fn)
-      | OnKeyUp fn -> ("onKeyUp", Js.Unsafe.inject @@ Js.Unsafe.callback fn)
-      | Key v -> ("key", Js.Unsafe.inject @@ v)
-      (* | Ref v -> ("ref", Js.Unsafe.inject @@ v) *)
-      | Style v -> ("style", Js.Unsafe.inject @@ v)
-      | Value v -> ("value", Js.Unsafe.inject @@ v)
-      | ClassName v -> ("className", Js.Unsafe.inject @@ v)
-
-    let create_js_obj props_to_attr_fn attrs = List.map props_to_attr_fn attrs
+      | Display s -> ("display", Js.Unsafe.inject s)
+      | Width s -> ("width", Js.Unsafe.inject s)
+      | Height s -> ("height", Js.Unsafe.inject s)
+      | BackgroundColor s -> ("backgroundColor", Js.Unsafe.inject s)
+      | Color s -> ("color", Js.Unsafe.inject s)
+      | TextDecoration s -> ("textDecoration", Js.Unsafe.inject s)
+    let create_js_obj attrs = List.map props_to_attr attrs
       |> Array.of_list
       |> Js.Unsafe.obj
+    let create attrs: Dom_html.cssStyleDeclaration Js.t = create_js_obj attrs
+  end
 
-    let create_reg_attr attr = create_js_obj props_to_attr attr
+  module Attr = struct
+      type mouse_event = <
+        nativeEvent: Dom_html.mouseEvent Js.t Js.readonly_prop;
+        preventDefault: unit -> unit Js.meth
+      > Js.t
+      type keyboard_event = <
+        nativeEvent: Dom_html.keyboardEvent Js.t Js.readonly_prop;
+        preventDefault: unit Js.meth
+      > Js.t
+
+      type 'a attr =
+        | OnClick of (mouse_event -> unit)
+        | OnContextMenu of (mouse_event -> unit)
+        | OnInput of (keyboard_event -> unit)
+        | OnKeyDown of (keyboard_event -> unit)
+        | OnKeyUp of (keyboard_event -> unit)
+        | Key of string
+        | Ref of 'a Js.Opt.t mut_ref_object
+        | Value of string
+        | Style of Dom_html.cssStyleDeclaration Js.t
+        | ClassName of string
+
+      let props_to_attr = let open Js.Unsafe in function
+        | OnClick fn -> ("onClick", inject @@ callback fn)
+        | OnContextMenu fn -> ("onContextMenu", inject @@ callback fn)
+        | OnInput fn -> ("onChange", inject @@ callback fn)
+        | OnKeyDown fn -> ("onKeyDown", inject @@ callback fn)
+        | OnKeyUp fn -> ("onKeyUp", inject @@ callback fn)
+        | Key v -> ("key", inject @@ v)
+        | Ref v -> ("ref", inject @@ v)
+        | Style v -> ("style", inject @@ v)
+        | Value v -> ("value", inject @@ v)
+        | ClassName v -> ("className", inject @@ v)
+
+      let create_js_obj props_to_attr_fn attrs = List.map props_to_attr_fn attrs
+        |> Array.of_list
+        |> Js.Unsafe.obj
+
+      let create_reg_attr attr = create_js_obj props_to_attr attr
+  end
 end
 
-let html_factory tag at children = create_element
+let html_factory tag at children = create_element ~children
   ~component:(HtmlTagComponent tag)
-  ~props:(Attr.create_reg_attr at)
-  ~children
+  ~props:(Html.Attr.create_reg_attr at)
   ~props_of_js:None
-let div (at: Dom_html.divElement Attr.attr list) ch = html_factory "div" at ch
 
-let fc
-  (type a)
+let div (at: Dom_html.divElement Html.Attr.attr list) ch = html_factory "div" at ch
+let span (at: Dom_html.htmlElement Html.Attr.attr list) ch = html_factory "span" at ch
+let p (at: Dom_html.paragraphElement Html.Attr.attr list) ch = html_factory "p" at ch
+
+let fc (type a)
   (module ReactComponent: FC with type t = a)
   (props: a) children
-= create_element
+= create_element ~children
   ~component:(FunctionalComponent ReactComponent.make)
-  ~props:(ReactComponent.t_to_js props) ~children
+  ~props:(ReactComponent.t_to_js props)
   ~props_of_js:(Some ReactComponent.t_of_js)
 
+let fragment ?key children =
+  let open Js.Unsafe in
+  let props = match key with
+    | Some s -> inject @@ object%js val key = Js.string s end
+    | None -> inject UndefOpt.empty
+  in
+  create_element ~props ~children
+    ~component:(FragmentComponent r_fragment)
+    ~props_of_js:None
+
+let provider (type a)
+  (module Provider: PROVIDER with type v = a)
+  value children =
+  let props = Provider.{value} |> Provider.t_to_js in
+  create_element ~props ~children
+    ~component:(ProviderComponent Provider.provider)
+    ~props_of_js:None
+
 let string str: element = Ojs.string_to_js str |> Obj.magic
+let int int: element = Ojs.int_to_js int |> Obj.magic
 let null: element = Js.Unsafe.pure_js_expr "null"
+
+(* Hooks *)
+let use_effect (fn: unit -> (unit -> unit) Js.optdef): unit = let open Js.Unsafe in
+  fun_call r_useEffect [|inject @@ callback fn|]
+
+let use_effect0 (fn: unit -> (unit -> unit) Js.optdef): unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback fn; inject @@ Js.array [||]|]
+
+let use_effect1 (fn: unit -> (unit -> unit) Js.optdef) d1 : unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback (fn); inject @@ Js.array [|d1|]|]
+
+let use_effect2 (fn: unit -> (unit -> unit) Js.optdef) ((d1,d2): ('a * 'b)) : unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback fn; inject @@ Js.array ([|d1;d2|] |> Array.map inject)|]
+
+let use_effect3 (fn: unit -> (unit -> unit) Js.optdef) ((d1,d2,d3): ('a * 'b * 'c)) : unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback fn; inject @@ Js.array ([|d1;d2;d3|] |> Array.map inject)|]
+
+let use_effect4 (fn: unit -> (unit -> unit) Js.optdef) ((d1,d2,d3,d4): ('a * 'b * 'c * 'd)) : unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback fn; inject @@ Js.array ([|d1;d2;d3;d4|] |> Array.map inject)|]
+
+let use_effect5 (fn: unit -> (unit -> unit) Js.optdef) ((d1,d2,d3,d4,d5): ('a *'b *'c *'d *'e)) : unit = let open Js.Unsafe in
+  fun_call r_useEffect
+  [|inject @@ callback fn; inject @@ Js.array ([|d1;d2;d3;d4;d5|] |> Array.map inject)|]
+
+let use_state (init_value: unit -> 'a): ('a * (('a -> 'a) -> unit)) =
+  let open Js.Unsafe in
+  let state_tuple = fun_call r_useState [|inject @@ callback init_value|] in
+  (get state_tuple "0", get state_tuple "1")
+
+let use_reducer
+  ?init:(init = fun (x: 'a) -> x)
+  (reducer: 'a -> 'b -> 'a)
+  (init_value: 'a)
+:('a * ('b -> unit)) =
+  let open Js.Unsafe in
+  let state_tuple = fun_call r_useReducer
+    [|inject @@ callback reducer;
+      inject @@ inject init_value;
+      inject @@ callback init|] in
+  (get state_tuple "0", get state_tuple "1")
+
+let use_callback0 (fn: 'a -> 'b): 'a -> 'b = Js.Unsafe.fun_call (Js.Unsafe.js_expr "React.useCallback")
+  [|Js.Unsafe.inject @@ Js.Unsafe.callback fn; Js.Unsafe.inject @@ Js.array [||]|]
+
+let use_callback1 (fn: 'a -> 'b) (d1: 'c): 'a -> 'b = Js.Unsafe.fun_call (Js.Unsafe.js_expr "React.useCallback")
+  [|Js.Unsafe.inject @@ Js.Unsafe.callback fn; Js.Unsafe.inject @@ Js.array [|d1|]|]
+
+let use_callback2 (fn: 'a -> 'b) ((d1, d2): ('c * 'd)): 'a -> 'b = Js.Unsafe.fun_call (Js.Unsafe.js_expr "React.useCallback")
+  [|Js.Unsafe.inject @@ Js.Unsafe.callback fn;
+    Js.Unsafe.inject @@ Js.array [|Js.Unsafe.inject d1; Js.Unsafe.inject d2|]|]
+
+let use_callback3 (fn: 'a -> 'b) ((d1, d2, d3): ('c * 'd * 'e)): 'a -> 'b = Js.Unsafe.fun_call
+  (Js.Unsafe.js_expr "React.useCallback") [|Js.Unsafe.inject @@ Js.Unsafe.callback fn;
+    Js.Unsafe.inject @@ Js.array [|Js.Unsafe.inject d1; Js.Unsafe.inject d2; Js.Unsafe.inject d3|]|]
+
+(* Others *)
+let memo (fn: 'a -> element): 'a -> element =
+  Js.Unsafe.fun_call (Js.Unsafe.js_expr "React.memo") [|Js.Unsafe.inject @@ Js.Unsafe.callback fn|]
 
 module Dom = struct
   let render (element: element) (node: Dom_html.element Js.t ): Dom_html.element Js.t =
-    Js.Unsafe.fun_call render
-    [|Js.Unsafe.inject element;
-      Js.Unsafe.inject node|]
+    let open Js.Unsafe in
+    fun_call rd_render [|inject element; inject node|]
 end
